02d63482e09c0160077f5dd183cb74f9f2b9b383
Dulana Peiris <57042272+du1ana@users.noreply.github.com>
2020-03-21 21:47:04 +0530

    Update script.js
    Format numeric output

--- script.js
+++ script.js
@@ -15,24 +15,28 @@ fetch('https://hpb.health.gov.lk/api/get-current-statistical')
 	
 
 		
-document.getElementById("update_date_time").innerHTML=sldata.update_date_time;
-	document.getElementById("local_new_cases").innerHTML=sldata.local_new_cases;
+document.getElementById("local_new_cases").innerHTML=sldata.local_new_cases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 		
-document.getElementById("local_total_cases").innerHTML=sldata.local_total_cases;
-	document.getElementById("local_active_cases").innerHTML=sldata.local_total_cases-sldata.local_deaths-sldata.local_recovered;
-		document.getElementById("local_deaths").innerHTML=sldata.local_deaths;
-		document.getElementById("local_recovered").innerHTML=sldata.local_recovered;
+document.getElementById("local_total_cases").innerHTML=sldata.local_total_cases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 	
-console.log(sldata);
+document.getElementById("local_active_cases").innerHTML=(sldata.local_total_cases-sldata.local_deaths-sldata.local_recovered).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		
+document.getElementById("local_deaths").innerHTML=sldata.local_deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		
+document.getElementById("local_recovered").innerHTML=sldata.local_recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	
+//console.log(sldata);
 	
 document.getElementById("local_progress").max=sldata.local_total_cases-sldata.local_deaths;
 document.getElementById("local_progress").value=sldata.local_recovered;
 
-document.getElementById("global_total_cases").innerHTML=sldata.global_total_cases;	
-	document.getElementById("global_active_cases").innerHTML=sldata.global_total_cases-sldata.global_deaths-sldata.global_recovered;
-		document.getElementById("global_deaths").innerHTML=sldata.global_deaths;
+document.getElementById("global_total_cases").innerHTML=sldata.global_total_cases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-document.getElementById("global_recovered").innerHTML=sldata.global_recovered;
+document.getElementById("global_active_cases").innerHTML=(sldata.global_total_cases-sldata.global_deaths-sldata.global_recovered).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+document.getElementById("global_deaths").innerHTML=sldata.global_deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+document.getElementById("global_recovered").innerHTML=sldata.global_recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
 document.getElementById("global_progress").max= sldata.global_total_cases-sldata.global_deaths;
 document.getElementById("global_progress").value=sldata.global_recovered;
